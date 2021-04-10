7069a7283a704c9f8f047cf0736e2bda62942fd8
Konstantin S <HavenDV@users.noreply.github.com>
2021-04-11 07:46:19 +1000

    feat: Added FluentAssertions. Added sync test.

--- src/tests/$PROJECT_NAME$.UnitTests/Tests.cs
+++ src/tests/$PROJECT_NAME$.UnitTests/Tests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
 
 namespace $PROJECT_NAME$.UnitTests
 {
@@ -9,12 +10,18 @@ namespace $PROJECT_NAME$.UnitTests
     public class Tests
     {
         [TestMethod]
-        public async Task DelayTest()
+        public async Task AsyncTest()
         {
             using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
             var cancellationToken = cancellationTokenSource.Token;
 
             await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+        }
+		
+        [TestMethod]
+        public void Test()
+        {
+			"".Should().Be("");
         }
     }
 }
